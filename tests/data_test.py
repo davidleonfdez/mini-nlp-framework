81d43b1bdf36f10b4ecec1afea114022f3dcf53b
davidleonfdez <dleoferu@gmail.com>
2022-03-28 18:49:38 +0100

    Add missing test

---
+++ tests/data_test.py
@@ -0,0 +1,44 @@
+from mini_nlp_framework.data import EmbeddingsSource, encode_text, spacy_pipeline_cache
+import pytest
+
+
+@pytest.mark.slow
+def test_encode_text():
+    corpus = [
+        "The 1st sequence.",
+        "This is the second document. Make it a bit longer to be sure it doesn't have padding.",
+        "The third sequence, finally."
+    ]
+    custom_result = encode_text(corpus, EmbeddingsSource.Std)
+    bert_result = encode_text(corpus, EmbeddingsSource.DistilBert)
+
+    nlp = spacy_pipeline_cache.get('en_core_web_lg')
+    minimum_expected_tokens = [
+        "The", "1st", "sequence.", "This", "is", "second", "document.", "Make", "it", "a", "bit", "longer."
+        "third", " "
+    ]
+    expected_tokens = [token.text for doc in corpus for token in nlp(doc) if token.has_vector] + [" "]
+
+    assert len(custom_result.vocab) >= len(minimum_expected_tokens)
+    assert set(custom_result.vocab.idx_to_word) == set(expected_tokens)
+    assert custom_result.X_padded[0][0] == custom_result.X_padded[2][0]
+    # It's not expected than any of {"This", "is"} is further subdivided, so we can
+    # check if the id of the second token of the second sequence is right
+    assert custom_result.X_padded[1][1] == custom_result.vocab.word_to_idx['is']
+    assert custom_result.vocab.pad_idx in custom_result.X_padded[0]
+    assert custom_result.vocab.pad_idx not in custom_result.X_padded[1]
+    assert custom_result.vocab.pad_idx in custom_result.X_padded[2]
+    assert custom_result.seq_lengths[1] > custom_result.seq_lengths[0]
+    assert custom_result.seq_lengths[1] > custom_result.seq_lengths[2]
+
+    assert len(bert_result.vocab) >= len(minimum_expected_tokens)
+    assert bert_result.X_padded[0][0] == bert_result.X_padded[2][0]
+    # It's not expected than any of {"This", "is"} is further subdivided, so we can
+    # check if the id of the third token of the second sequence is right (the first 
+    # token is always [CLS])
+    assert bert_result.X_padded[1][2] == bert_result.vocab.word_to_idx['is']
+    assert bert_result.vocab.pad_idx in bert_result.X_padded[0]
+    assert bert_result.vocab.pad_idx not in bert_result.X_padded[1]
+    assert bert_result.vocab.pad_idx in bert_result.X_padded[2]
+    assert bert_result.seq_lengths[1] > bert_result.seq_lengths[0]
+    assert bert_result.seq_lengths[1] > bert_result.seq_lengths[2]
